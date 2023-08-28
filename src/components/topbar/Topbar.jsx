18d2dbaa90fb7e3e4d570a604b223d95f4140b9f
unknown <yusha814@gmail.com>
2023-08-28 21:55:14 +0600

    left topbar completed

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {Person, Mail} from "@mui/icons-material"
 
 export default function Topbar() {
   return (
@@ -6,7 +7,14 @@ export default function Topbar() {
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">Logo</a>
-          
+          <div className="itemContainer">
+            <Person className="icons"/>
+            <span>+88 01785565728</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icons"/>
+            <span>yusha.abd@gmail.com</span>
+          </div>
         </div>
         <div className="right">
           
