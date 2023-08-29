fad1494b0aa87c1b64cdbc4ed6d278c179a09597
unknown <yusha814@gmail.com>
2023-08-30 00:23:34 +0600

    portfolio page added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,40 @@ import "./portfolio.scss"
 
 export default function Portfolio() {
   return (
-    <div className="portfolio" id="portfolio">Portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Games</li>
+        <li>Machine Learning</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+        <div className="item">
+          <img src={process.env.PUBLIC_URL + "/assets/projects/Saga Event.png"} alt="" />
+          <h3>Event Mangaement App</h3>
+        </div>
+      </div>
+    </div>
   )
 }
